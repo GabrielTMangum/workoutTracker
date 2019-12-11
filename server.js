aa18272bd9e47d6511384d777e4b9830aa51761b
GabrielTMangum <gabrieltmangum@gmail.com>
2019-12-11 16:07:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`app running on http://localhost:${PORT}/`)
+})
