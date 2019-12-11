3d6b084995de820bd82c268406d5b463191066ce
GabrielTMangum <gabrieltmangum@gmail.com>
2019-12-11 17:49:40 -0500

    simplified schema and began work on input fields and buttons in index.html

--- models/Workout.js
+++ models/Workout.js
@@ -3,16 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
-  },
-  activities: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Activity"
-    }
-]
+  name: String,
+  body: String
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+const db = require("./models/Workout");
 
 const app = express();
 
@@ -23,6 +23,27 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"./public/index.html"))
 })
+
+app.post('/submit', ({body}, res) => {
+    const newWorkout = new Workout(body)
+    db.create(newWorkout)
+        .then( data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+app.get('/list', (req, res) => {
+    Workout.find({})
+        .then( data => {
+            res.json(data)
+        })
+        .cath( err => {
+            res.json(err)
+        })
+});
 
 app.listen(PORT, () => {
     console.log(`app running on http://localhost:${PORT}/`)
